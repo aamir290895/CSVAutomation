e5457340032ea8b1dcea0f4b3213c3cdf4de144c
aamir290895 <aamir.007sdl.ak@gmail.com>
2022-04-11 01:03:57 +0530

    11/04/2022

--- CSVAutomation/src/main/java/com/java/automatecsv/CSVAutomation/App.java
+++ CSVAutomation/src/main/java/com/java/automatecsv/CSVAutomation/App.java
@@ -1,14 +1,23 @@
 package com.java.automatecsv.CSVAutomation;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.opencsv.exceptions.CsvException;
+
+import Config.WriteCSV;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FileNotFoundException, CsvException, IOException
     {
     	
+    	WriteCSV.writeCSV();
     	
+    	System.out.println("Congratulations ! your output csv file generate.......");
     }
 }
